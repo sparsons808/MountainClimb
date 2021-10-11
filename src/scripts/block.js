6a1953ece20f6bdfe5d7f44dbfa33f94ec7ea679
Sydney <sydneyparsonsyb@gmail.com>
2021-10-11 10:54:37 -0400

    stopped working

---
+++ src/scripts/block.js
@@ -0,0 +1,16 @@
+export class Block {
+    constructor(game, position) {
+        this.game = game;
+        this.position = position;
+
+        this.blockblock = new Image()
+        this.block.src = '../assets/block.png'
+
+        this.width = 35;
+        this.height = 25;
+    }
+
+    draw(ctx) {
+        ctx.draw
+    }
+}
