a8f25ed8af4ef6127125050f73baf981b8addf1d
Sydney <sydneyparsonsyb@gmail.com>
2021-10-12 13:38:43 -0400

    collison half working

--- src/scripts/collision.js
+++ src/scripts/collision.js
@@ -1,15 +1,15 @@
 export function collison(sprite, object) {
     
-    // let topOfSprite = sprite.position.y
+    let topOfSprite = sprite.position.y
     
-    // let bottomOfSprite = sprite.position.y + sprite.height
-    // let leftOfSprite = sprite.position.x
-    // let rightOfSprite = sprite.position.x + sprite.width
+    let bottomOfSprite = sprite.position.y + sprite.height
+    let leftOfSprite = sprite.position.x
+    let rightOfSprite = sprite.position.x + sprite.width
     
-    // let topOfObject = object.position.y
-    // let bottomOfObject = object.position.y + object.height
-    // let leftSideOfObject = object.position.x
-    // let rightSideOfObject = object.position.x + object.width
+    let topOfObject = object.position.y
+    let bottomOfObject = object.position.y + object.height
+    let leftSideOfObject = object.position.x
+    let rightSideOfObject = object.position.x + object.width
 
     // if(topOfSprite >= bottomOfObject &&
     //     bottomOfSprite <= topOfObject &&
@@ -19,19 +19,27 @@ export function collison(sprite, object) {
     // } else return false;
 
     let xDistanceCenter = sprite.width / 2 + object.width / 2 - 4
-    let yDistanceCenter = sprite.height / 2 + object.height / 2 - 4
+    let yDistanceCenter = sprite.height / 2 + object.height / 2 - 1
 
-    if(distanceFromObject(sprite, object) < xDistanceCenter || distanceFromObject(sprite, object) < yDistanceCenter) {
-        sprite.speedY = 0
-        
+    if(topOfSprite === bottomOfObject){
+        sprite.speedY = sprite.speedY
     }
+
+    
+    if(distanceFromObject(sprite, object) < yDistanceCenter && distanceFromObject(sprite, object) < xDistanceCenter) {
+        sprite.speedY = 0
+        sprite.position.y = topOfObject - sprite.height
+    }
+    // } else if (distanceFromObject(sprite, object) > yDistanceCenter && distanceFromObject(sprite, object) < xDistanceCenter){
+    //     // console.log('hit')
+    // }
 }
 
 // us parag
 
 function distanceFromObject(sprite, object) {
-    let horizontalDistance = object.position.x - sprite.position.x 
-    let verticalDistance = object.position.y - sprite.position.y
+    let horizontalDistance = (object.position.x + object.width / 2) - (sprite.position.x + sprite.width / 2) 
+    let verticalDistance = (object.position.y + object.height / 2) - (sprite.position.y + sprite.height / 2)
 
     return Math.sqrt(Math.pow(horizontalDistance, 2) + Math.pow(verticalDistance, 2));
 }
